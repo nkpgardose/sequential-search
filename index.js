4043dc987791ac08122f0bad24fead2076a17143
Neil Kim Gardose <neil.sprdout@gmail.com>
2015-01-07 13:57:27 +0800

    Add module and simple testing

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+/**
+ * Sequential Search. Return '-1' if not found.
+ * @param {list} Set of data to find index on
+ * @param {key} Value to compare to get index
+ */
+function sequentialSearch(list, key, size) {
+  var count = 0;
+  while(count < size) {
+    if(list[count] === key) {
+      return count;
+    }
+    count++;
+  }
+
+  return -1;
+}
+
+module.exports = function (list, key) {
+  return sequentialSearch(list, key, list.length);
 };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,26 @@
 /*global describe, it */
 'use strict';
 var assert = require('assert');
-var sequentialSearch = require('../');
+var sequential = require('../');
 
 describe('sequential-search node module', function () {
-  it('must have at least one test', function () {
-    sequentialSearch();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+  it('should hit array values in given length', function() {
+    assert.deepEqual(sequential([2, 4, 3, 6, 7, 8], 3), 2);
+    assert.deepEqual(sequential([2, 4, 3, 6, 7, 8], 7), 4);
+    assert.deepEqual(sequential([2, 4, 3, 6, 7, 8], 4), 1);
+  });
+
+  it('should work with arrays with 2 elements', function() {
+    assert.deepEqual(sequential([1, 2], 2), 1);
+    assert.deepEqual(sequential([1, 2], 1), 0);
+  });
+
+  it('should return -1 for missing elements', function() {
+    assert.deepEqual(sequential([1, 2, 3], 4), -1);
+    assert.deepEqual(sequential([1, 2, 3], 5), -1);
+  });
+
+  it('should work in empty arrays', function(){
+    assert.deepEqual(sequential([], 4), -1);
   });
 });
